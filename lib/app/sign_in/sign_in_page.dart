8d22a4382d3d98edf94dabeecc5627211e9b5ba5
westsider <whansen1@mac.com>
2022-05-05 15:35:35 -0500

    added button

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -17,27 +17,20 @@ class SignInPage extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.all(16.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            color: Colors.orange,
-            child: SizedBox(
-              height: 100,
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
             ),
           ),
           SizedBox(height: 8.0),
-          Container(
-            color: Colors.red,
-            child: SizedBox(
-              height: 100,
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Container(
-            color: Colors.purple,
-            child: SizedBox(
-              height: 100,
-            ),
+          ElevatedButton(onPressed: () { print('button pressed');},
+              child: Text('Sign in with Google'),
           ),
         ],
       ),
